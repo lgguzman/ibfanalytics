12511721e857753d985a8af5c2f56690dbf70678
Luis Guzman <ing.luis.guzman.reyes@gmail.com>
2018-10-29 17:30:11 -0500

    cluster by user behaivor

--- ibfanalytics/user_cluster.py
+++ ibfanalytics/user_cluster.py
@@ -2,8 +2,10 @@ from sbxpy import SbxCore
 import asyncio
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import mean as _mean, stddev as _stddev, col
+from pyspark.sql.functions import mean as _mean, stddev as _stddev, col, to_json, from_json, struct, lit
 from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField
+from pyspark.ml.linalg import VectorUDT
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 import matplotlib.pyplot as plt
@@ -11,6 +13,59 @@ from pyspark.ml.feature import PCA as PCAml
 
 
 class UserCluster:
+
+
+    async def df_sbx_customer_purchase_behaivor(self, sbx, spark, date_int, limit =3):
+        data_complete = await  sbx.with_model('cart_box_item').fetch_models(['cart_box']) \
+            .and_where_greater_than('cart_box.charge_date', date_int) \
+            .and_where_is_not_null('cart_box.purchase').find_all_query()
+        sc = spark.sparkContext
+        d = {}
+        groups = {}
+        errors = []
+
+        for data in  data_complete:
+            for item in data['results']:
+                try:
+                    customer = data['fetched_results']['cart_box'][item['cart_box']]['customer']
+                    if customer not in d:
+                        d[customer] = {}
+                    purchase = item['product_group'] + ' ' +item['variety']
+                    if purchase not in d[customer]:
+                        d[customer][purchase] = 1
+                    if purchase not in groups:
+                        groups[purchase] = 1
+                except Exception as inst:
+                    errors.append(inst)
+
+        columns = [key for key, value in groups.items()]
+        def newRow(data, customer, columns):
+            row = {}
+            row['customer'] = customer
+            row['products'] = []
+            for key  in columns:
+                if key not in data:
+                    row['products'].append(0)
+                else:
+                    row['products'].append(1)
+            return row
+
+        customers = [ newRow(value, key, columns) for key, value in d.items()]
+        customers = list(filter(lambda x: sum(x['products']) > limit, customers) )
+
+        tmp = sc.parallelize(customers, numSlices=100)
+        df = spark.read.option("multiLine", "true").json(tmp)
+        json_vec = to_json(struct(struct(
+            lit(1).alias("type"),  # type 1 is dense, type 0 is sparse
+            col("products").alias("values")
+        ).alias("v")))
+
+        schema = StructType([StructField("v", VectorUDT())])
+
+        return df.withColumn(
+            "features", from_json(json_vec, schema).getItem("v")
+        )
+
 
     async def df_sbx_customer_special_box_purchased(self, sbx, spark):
         data = await  sbx.with_model('cart_box') \
@@ -74,14 +129,17 @@ class UserCluster:
     async def test(self):
         sbx = SbxCore()
         sbx.initialize(os.environ['DOMAIN'], os.environ['APP-KEY'], os.environ['SERVER_URL'])
-        login = await sbx.login(os.environ['LOGIN'], os.environ['PASSWORD'], os.environ['DOMAIN'] )
+        login = await sbx.login(os.environ['LOGIN'], os.environ['PASSWORD'], os.environ['DOMAIN2'] )
 
         spark = SparkSession.builder.appName("SparkExample").getOrCreate()
 
         df = await self.df_sbx_customer_special_box_purchased(sbx,spark)
+        df.show()
+
         model = await self.k_means(df,3,1)
         transformed = await self.run_cluster(model, df)
-        transformed.select(['customer','current_percentage', 'count' , 'total_items', 'prediction']).show(20, False)
+
+        # transformed.select(['customer','current_percentage', 'count' , 'total_items', 'prediction']).show(20, False)
 
         await self.plot_cluster(transformed)
         spark.stop()
@@ -89,4 +147,3 @@ class UserCluster:
     def run_test(self):
         loop = asyncio.new_event_loop()
         loop.run_until_complete(self.test())
-
--- scripts.py
+++ scripts.py
@@ -1,5 +1,6 @@
 from subprocess import call
 call("python3 -m pip install -r requirements.txt --upgrade --target dependencies", shell=True)
+call("rm -r dependencies/*.dist-info", shell=True)
 call("zip dependencies.zip dependencies", shell=True)
 
 
